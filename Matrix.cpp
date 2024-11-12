80c745c61de39be104dc0e30e3e74a4faf224a11
Felixo <felixoosol@gmail.com>
2024-11-12 12:49:44 +0100

    Permutation checkers

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,10 +3,11 @@
 #include <numeric>
 #include <iomanip>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-// Converts an integer to a binary vector of size `n`
+// De int a binario en un vector de tamaño n
 vector<int> toBinary(int value, int n) {
     vector<int> v(n);
     for (int i = 0; i < n; ++i) {
@@ -20,30 +21,41 @@ int main() {
     cout << "Introduzca n (Tamaño de la matriz: 2^n): ";
     cin >> n;
     int size = static_cast<int>(pow(2, n));
-
-    // Get the permutation vector from the user
     vector<int> permutation(size);
     cout << "Introduzca la permutación de tamaño " << size << " (separado por espacios): ";
     for (int i = 0; i < size; ++i) {
         cin >> permutation[i];
     }
 
-    // Define dynamic 2D matrices
+
+
+    // Comprobación : Verificar que los valores sean únicos y estén en el rango correcto
+    vector<int> expected(size);
+    iota(expected.begin(), expected.end(), 0); 
+    vector<int> sorted_permutation = permutation; 
+    sort(sorted_permutation.begin(), sorted_permutation.end());
+
+    if (sorted_permutation != expected) {
+        cout << "Error: La permutación debe contener todos los valores únicos desde 0 hasta " << size - 1 << ".\n";
+        return 1;
+    }
+    
+    // Definir matrices
     vector<vector<int>> matriz(size, vector<int>(size));
     vector<vector<int>> matriz2(size, vector<int>(size));
     vector<vector<int>> mult(size, vector<int>(size, 0));
 
-    // Fill matriz and matriz2 based on toBinary function and permutation
+    // Rellenar matrices
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             vector<int> v1 = toBinary(i, n);
-            vector<int> v2 = toBinary(permutation[j % permutation.size()], n); // Use modulo to avoid out-of-bounds
+            vector<int> v2 = toBinary(permutation[j], n); 
             matriz[i][j] = pow(-1, inner_product(v1.begin(), v1.end(), v2.begin(), 0));
-            matriz2[j][i] = matriz[i][j];  // Transpose
+            matriz2[j][i] = matriz[i][j]; //Traspuesta
         }
     }
 
-    // Multiply matriz and matriz2 and store result in mult
+    // Multiplicar matrices
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
             for (int k = 0; k < size; ++k) {
@@ -52,7 +64,7 @@ int main() {
         }
     }
 
-    // Print matriz
+    // Print Matriz
     cout << "Matriz:\n";
     for (const auto& row : matriz) {
         for (int val : row) {
@@ -62,8 +74,8 @@ int main() {
     }
     cout << endl;
 
-    // Print matriz2 (transposed matrix)
-    cout << "Matriz2 (Transposed):\n";
+    // Print Matriz2 (Traspuesta)
+    cout << "Matriz2 (Traspuesta):\n";
     for (const auto& row : matriz2) {
         for (int val : row) {
             cout << setw(3) << val << " ";
@@ -72,8 +84,8 @@ int main() {
     }
     cout << endl;
 
-    // Print mult (result of multiplication)
-    cout << "Multiplication Result:\n";
+    // Print mult (matriz x matriz2)
+    cout << "Matriz x Matriz2:\n";
     for (const auto& row : mult) {
         for (int val : row) {
             cout << setw(3) << val << " ";
